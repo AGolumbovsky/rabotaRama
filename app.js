088b5099959d352b430f06c67fae7a1699179d73
AGolumbovsky <agolumbovsky@gmail.com>
2015-09-17 19:35:17 -0500

    commit

--- app.js
+++ app.js
@@ -4,4 +4,8 @@
         return new Greetr.init(firstName, lastName, language);
     }
     
+    Greetr.init = function(firstName, lastName, language) {
+        
+    }
+    
 }(window, jQery));return new Greetr.init(firstName, lastName, language);
