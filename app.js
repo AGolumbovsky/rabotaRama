f8d5196b26e4831072883c1fa8d88979d1fda032
AGolumbovsky <agolumbovsky@gmail.com>
2015-09-19 13:47:45 -0500

    rr

--- app.js
+++ app.js
@@ -1,11 +1,9 @@
-(function (global, $) {
-    
-    var Greetr = function (firstName, lastName, language) {
+(function(global, $) {
+    var Greetr = function(firstName, lastName, language) {
         return new Greetr.init(firstName, lastName, language);
     }
     
-    Greetr.init = function(firstName, lastName, language) {
+    Greetr.init = function (firstName, lastName, language) {
         
     }
-    
-}(window, jQery));return new Greetr.init(firstName, lastName, language);
+})
